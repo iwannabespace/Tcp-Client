dece1fa432291f3eb158a85cacc4701071c7e135
Ertuğrul Faruk Pişkin <thertu58@gmail.com>
2025-01-13 14:33:23 +0300

    fixes

--- src/client.rs
+++ src/client.rs
@@ -1,19 +1,20 @@
+use std::net::Shutdown;
+use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread;
 use std::{
     io::{self, Read, Write},
     net::TcpStream,
-    process::exit,
     sync::{Arc, Mutex},
     time::Duration,
 };
 
-use crate::{Message, MessageType};
-
-type OnMessageReceivedCallback = Arc<Mutex<Box<dyn Fn(Message) + Send>>>;
+type OnMessageReceivedCallback = Arc<Mutex<Box<dyn Fn(&[u8]) + Send>>>;
 
 pub struct TcpClientData {
     socket: TcpStream,
     on_message_received: OnMessageReceivedCallback,
+    sender: Sender<bool>,
+    receiver: Mutex<Receiver<bool>>,
 }
 
 pub struct TcpClient {
@@ -31,9 +32,13 @@ impl TcpClientData {
                     return Err("Couldn't set socket to nonblocking mode".to_string());
                 };
 
+                let (sender, receiver) = channel::<bool>();
+
                 Ok(Self {
                     socket,
                     on_message_received: Arc::new(Mutex::new(Box::new(|_| {}))),
+                    sender,
+                    receiver: Mutex::new(receiver),
                 })
             }
             Err(e) => Err(format!("Error on connection: {e}")),
@@ -54,54 +59,61 @@ impl TcpClient {
         }
     }
 
-    pub fn send(&self, message: Message) -> Result<(), String> {
+    pub fn disconnect(&self) -> bool {
+        let result = self.data.socket.shutdown(Shutdown::Both);
+        let data_ref = self.data.clone();
+
+        if result.is_ok() {
+            let _ = data_ref.sender.send(true);
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn send(&self, data: &[u8]) -> Result<(), String> {
         let data_ref = self.data.clone();
         let mut socket = &data_ref.socket;
 
-        match bincode::serialize(&message) {
-            Ok(data) => {
-                let header = (data.len() as u64).to_le_bytes();
-                let mut header_written: usize = 0;
-                let mut body_written: usize = 0;
+        let header = (data.len() as u64).to_le_bytes();
+        let mut header_written: usize = 0;
+        let mut body_written: usize = 0;
 
-                while header_written < 8 {
-                    match socket.write(&header[header_written..]) {
-                        Ok(size) => {
-                            if size > 0 {
-                                header_written += size;
-                            }
-                        }
-                        Err(e) => {
-                            if e.kind() == io::ErrorKind::WouldBlock {
-                                thread::sleep(Duration::from_millis(50));
-                            } else {
-                                return Err(e.kind().to_string());
-                            }
-                        }
+        while header_written < 8 {
+            match socket.write(&header[header_written..]) {
+                Ok(size) => {
+                    if size > 0 {
+                        header_written += size;
                     }
                 }
-
-                while body_written < data.len() {
-                    match socket.write(&data[body_written..]) {
-                        Ok(size) => {
-                            if size > 0 {
-                                body_written += size;
-                            }
-                        }
-                        Err(e) => {
-                            if e.kind() == io::ErrorKind::WouldBlock {
-                                thread::sleep(Duration::from_millis(50));
-                            } else {
-                                return Err(e.kind().to_string());
-                            }
-                        }
+                Err(e) => {
+                    if e.kind() == io::ErrorKind::WouldBlock {
+                        thread::sleep(Duration::from_millis(50));
+                    } else {
+                        return Err(e.kind().to_string());
                     }
                 }
-
-                Ok(())
             }
-            Err(e) => Err(format!("Serialization error: {e}")),
         }
+
+        while body_written < data.len() {
+            match socket.write(&data[body_written..]) {
+                Ok(size) => {
+                    if size > 0 {
+                        body_written += size;
+                    }
+                }
+                Err(e) => {
+                    if e.kind() == io::ErrorKind::WouldBlock {
+                        thread::sleep(Duration::from_millis(50));
+                    } else {
+                        return Err(e.kind().to_string());
+                    }
+                }
+            }
+        }
+
+        Ok(())
     }
 
     pub fn receive(&self) {
@@ -115,6 +127,14 @@ impl TcpClient {
             let header_size = std::mem::size_of::<u64>();
 
             loop {
+                if let Ok(receiver) = data_ref.receiver.lock() {
+                    if let Ok(data) = receiver.try_recv() {
+                        if data {
+                            break;
+                        }
+                    }
+                }
+
                 if read_bytes >= 8 {
                     let arr: [u8; 8] = buffer[0..header_size].try_into().unwrap();
                     amount_to_read = usize::from_le_bytes(arr);
@@ -128,12 +148,8 @@ impl TcpClient {
                     Ok(size) => {
                         if size == 0 {
                             if let Ok(on_message_received) = data_ref.on_message_received.lock() {
-                                let message = Message {
-                                    message_type: MessageType::ConnectionDropped,
-                                    body: None,
-                                };
-                                on_message_received(message);
-                                exit(0);
+                                on_message_received(&[]);
+                                // exit(0);
                             }
                         } else {
                             read_bytes += size;
@@ -141,17 +157,7 @@ impl TcpClient {
                             if amount_to_read > 0 && read_bytes == header_size + amount_to_read {
                                 if let Ok(on_message_received) = data_ref.on_message_received.lock()
                                 {
-                                    if let Ok(message) =
-                                        bincode::deserialize::<Message>(&buffer[header_size..])
-                                    {
-                                        on_message_received(message);
-                                    } else {
-                                        let message = Message {
-                                            message_type: MessageType::MessageDeserializeError,
-                                            body: None,
-                                        };
-                                        on_message_received(message);
-                                    }
+                                    on_message_received(&buffer[header_size..]);
                                     buffer.resize(8, 0);
                                     read_bytes = 0;
                                     amount_to_read = 0;
@@ -175,7 +181,7 @@ impl TcpClient {
 
     pub fn set_on_message_received<F>(&mut self, callback: F)
     where
-        F: Fn(Message) + Send + 'static,
+        F: Fn(&[u8]) + Send + 'static,
     {
         if let Ok(mut cb) = self.data.on_message_received.lock() {
             *cb = Box::new(callback);
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
 mod client;
-mod custom_types;
 
 pub use client::*;
-pub use custom_types::*;
